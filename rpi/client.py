b94b9a2ca8307259fff6a14f5ddb6e727877b2e6
graffjosh <github@joshuagraff.com>
2017-04-07 12:40:04 -0400

    client works?

--- rpi/client.py
+++ rpi/client.py
@@ -1,15 +1,35 @@
-import cv2
 import numpy as np
+import cv2
 import socket
-import sys
-import pickle
-import struct ### new code
 
-cap=cv2.VideoCapture(0)
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('130.215.75.131',1234))
-while True:
-    ret,frame=cap.read()
-    data = pickle.dumps(frame) ### new code
-    cv2.imshow('frame',frame)
-    clientsocket.sendall(struct.pack("H", len(data))+data) ### new code
+UDP_IP = "130.215.75.131"
+UDP_PORT = 1234
+
+cam0 = cv2.VideoCapture(0)
+cam1 = cv2.VideoCapture(1)
+
+while(True):
+    ret, frame0 = cam0.read()
+    ret, frame1 = cam1.read()
+
+    d0 = frame0.flatten ()
+    s0 = d0.tostring ()
+
+    d1 = frame1.flatten()
+    s1 = d1.tostring()
+
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    for i in range(20):
+        sock.sendto (s0[i*46080:(i+1)*46080],(UDP_IP, UDP_PORT))
+
+
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    for i in range(20):
+        sock.sendto (s1[i*46080:(i+1)*46080],(UDP_IP, UDP_PORT))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam0.release()
+cam1.release()
+cv2.destroyAllWindows()
