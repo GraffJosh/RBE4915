f363e7e79b1013f678762042bf9a00be59fb3d11
graffjosh <gitlab@joshuagraff.com>
2017-04-07 13:21:22 -0400

    Janky dual feed

--- VideoStreaming/server.py
+++ VideoStreaming/server.py
@@ -6,25 +6,30 @@ import cv2
 UDP_IP = "130.215.75.131"
 UDP_PORT = 1234
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind ((UDP_IP, UDP_PORT))
-
+sock0 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock0.bind ((UDP_IP, 1234))
+sock1 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock1.bind ((UDP_IP, 1235))
 s=""
 
 while True:
 
-    data, addr = sock.recvfrom(46080)
+    data0, addr = sock0.recvfrom(46080)
+    data1, addr = sock1.recvfrom(46080)
 
-    s += data
+    s0 += data0
+    s1 += data1
 
-    if len(s) == (46080*40):
-
-        frame = numpy.fromstring (s,dtype=numpy.uint8)
-        frame = frame.reshape (480,1280,3)
-
-        cv2.imshow('frame',frame)
-
-        s=""
+    if len(s0) == (46080*20):
+        frame0 = numpy.fromstring (s0,dtype=numpy.uint8)
+        frame0 = frame0.reshape (480,680,3)
+        cv2.imshow('frame',frame0)
+        s0=""
+    if len(s1) == (46080*20):
+        frame1 = numpy.fromstring (s1,dtype=numpy.uint8)
+        frame1 = frame1.reshape (480,680,3)
+        cv2.imshow('frame',frame1)
+        s1=""
 
     if cv2.waitKey(1) & 0xFF == ord ('q'):
         break
--- rpi/client.py
+++ rpi/client.py
@@ -3,6 +3,12 @@ import cv2
 import socket
 
 def send(frame, IP, Port):
+    pass
+    d = frame.flatten ()
+    s = d.tostring ()
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    for i in range(20):
+        sock.sendto (s[i*46080:(i+1)*46080],(UDP_IP, UDP_PORT))
 
 
 
@@ -11,27 +17,28 @@ UDP_PORT = 1234
 
 cam0 = cv2.VideoCapture(0)
 cam1 = cv2.VideoCapture(1)
-stitcher = cv2.createStitcher(False)
 
 while(True):
     ret, frame0 = cam0.read()
     ret, frame1 = cam1.read()
 
+    send(frame0, UDP_IP, 1234)
+    send(frame1, UDP_IP, 1235)
     #d0 = frame0.flatten ()
     #s0 = d0.tostring ()
     #d1 = frame1.flatten()
     #s1 = d1.tostring()
 
 
-    stitched = np.concatenate((frame0,frame1),axis=1)
+    # stitched = np.concatenate((frame0,frame1),axis=1)
 
-    d = stitched.flatten ()
-    s = d.tostring ()
-    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-   
+    # d = stitched.flatten ()
+    # s = d.tostring ()
 
-    for i in range(40):
-        sock.sendto (s[i*46080:(i+1)*46080],(UDP_IP, UDP_PORT))
+    #
+    # sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    # for i in range(40):
+    #     sock.sendto (s[i*46080:(i+1)*46080],(UDP_IP, UDP_PORT))
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
