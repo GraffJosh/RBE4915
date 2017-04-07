a6857ae2bbb20f8a2f2b2b002e19cbc1866357da
graffjosh <github@joshuagraff.com>
2017-04-07 13:14:58 -0400

    Client

--- rpi/client.py
+++ rpi/client.py
@@ -2,39 +2,37 @@ import numpy as np
 import cv2
 import socket
 
+def send(frame, IP, Port):
+
+
+
 UDP_IP = "130.215.75.131"
 UDP_PORT = 1234
 
 cam0 = cv2.VideoCapture(0)
 cam1 = cv2.VideoCapture(1)
+stitcher = cv2.createStitcher(False)
 
 while(True):
     ret, frame0 = cam0.read()
     ret, frame1 = cam1.read()
 
-    d0 = frame0.flatten ()
-    s0 = d0.tostring ()
+    #d0 = frame0.flatten ()
+    #s0 = d0.tostring ()
+    #d1 = frame1.flatten()
+    #s1 = d1.tostring()
 
 
+    stitched = np.concatenate((frame0,frame1),axis=1)
 
-    merged_frame = Mat(Size(frame0.cols*2,frame0.rows), CV_8UC3)
-    roi = Mat(merged_frame, Rect(0, 0, frame0.cols, frame0.rows));
-    frame0.copyTo(roi);
-    roi = Mat(merged_frame, Rect(frame0.cols, frame0.rows,frame0.cols*2,frame0.rows));
-    frame1.copyTo(roi);
-
-    d1 = frame1.flatten()
-    s1 = d1.tostring()
-
-
+    d = stitched.flatten ()
+    s = d.tostring ()
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    for i in range(20):
-        sock.sendto (s0[i*46080:(i+1)*46080],(UDP_IP, UDP_PORT))
+   
 
+    for i in range(40):
+        sock.sendto (s[i*46080:(i+1)*46080],(UDP_IP, UDP_PORT))
 
-    # sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    # for i in range(20):
-    #     sock.sendto (s1[i*46080:(i+1)*46080],(UDP_IP, UDP_PORT))
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
