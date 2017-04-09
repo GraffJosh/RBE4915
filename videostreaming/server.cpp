335c661c322b67d77e88ed1d74df524a02e87d39
graffjosh <github@joshuagraff.com>
2017-04-09 17:50:26 +0000

    updated c++server

--- videostreaming/server.cpp
+++ videostreaming/server.cpp
@@ -1,116 +1,198 @@
-#include "highgui.h"
-#include "cv.h"
-#define CV_H
-#include <iostream>
-#include <conio.h>
-#include "UDPClient.h"
-using namespace std;
-using namespace cv;
+// ========================= SEVER =========================
 
-char *addr = "127.0.0.1";
-int port = 9999;
+/** \brief Initialize a UDP server object.
+ *
+ * This function initializes a UDP server object making it ready to
+ * receive messages.
+ *
+ * The server address and port are specified in the constructor so
+ * if you need to receive messages from several different addresses
+ * and/or port, you'll have to create a server for each.
+ *
+ * The address is a string and it can represent an IPv4 or IPv6
+ * address.
+ *
+ * Note that this function calls connect() to connect the socket
+ * to the specified address. To accept data on different UDP addresses
+ * and ports, multiple UDP servers must be created.
+ *
+ * \note
+ * The socket is open in this process. If you fork() or exec() then the
+ * socket will be closed by the operating system.
+ *
+ * \warning
+ * We only make use of the first address found by getaddrinfo(). All
+ * the other addresses are ignored.
+ *
+ * \exception udp_client_server_runtime_error
+ * The udp_client_server_runtime_error exception is raised when the address
+ * and port combinaison cannot be resolved or if the socket cannot be
+ * opened.
+ *
+ * \param[in] addr  The address we receive on.
+ * \param[in] port  The port we receive from.
+ */
 
-int main(){
-	cout<<"=== VIDEO SENDER ==="<<endl;
-
-	//setup UDP client:
-	UDPClient *client = new UDPClient(addr, port);
-
-	//setup openCV
-	cvNamedWindow("UDP Video Sender", CV_WINDOW_AUTOSIZE);
-	CvCapture* capture = cvCreateCameraCapture(0);
-	if(!capture){
-		cout<<"No camera found."<<endl;
-		goto DONE;
-	}
-	IplImage *frame;
-	frame = cvQueryFrame(capture);
-	IplImage *small = cvCreateImage(cvSize(frame->width / 2, frame->height / 2),
-		frame->depth, 3);
-
-	while(1){
-		//capture frame and resize
-		frame = cvQueryFrame(capture);
-		cvResize(frame, small, CV_INTER_LINEAR);
-		cvShowImage("UDP Video Sender", small);
-
-
-		//send
-		int result = client->sendData((char*)(&buff[0]), buff.size());
-		if(result < 0)
-			cout<<"Failed to send."<<endl;
-		else
-			cout<<"Sent a frame of size "<<result<<endl;
-
-		cvWaitKey(15);
-	}
-
-	goto DONE;
-
-DONE:
-	cout<<"Press any key to continue."<<endl;
-	getch();
-}
-
-/*
-int main()
+ #include "server.h"
+ using namespace udp_client_server;
+udp_server::udp_server(const std::string& addr, int port)
+    : f_port(port)
+    , f_addr(addr)
 {
-   cvNamedWindow( "Example2", CV_WINDOW_AUTOSIZE );
-   CvCapture* capture = cvCreateCameraCapture(0);
-
-   IplImage* frame;
-       frame = cvQueryFrame( capture );
-    if(!frame) return 0;
-    int width = frame->width;
-    int height = frame->height;
-
-   while(1){
-
-    IplImage* imgOld = cvCreateImage(cvSize(width/2,height/2),frame->depth,3);
-    cvResize(frame,imgOld,CV_INTER_LINEAR);
-    //cvCopy(frame1,img);
-  frame = cvQueryFrame( capture ); 
-    IplImage* imgNew = cvCreateImage(cvSize(width/2,height/2),frame->depth,3);
-    cvResize(frame,imgNew,CV_INTER_LINEAR);
-  cvAbsDiff(imgOld,imgNew,imgOld);
-  //cvAddS(frame1,cvScalar(150,150,150),frame1);
-
-  IplImage *im_gray = cvCreateImage(cvGetSize(imgOld),IPL_DEPTH_8U,1);
-  cvCvtColor(imgOld,im_gray,CV_RGB2GRAY);
-  
-
-  cvThreshold( im_gray, im_gray, 30, 30, CV_THRESH_TOZERO );
-  cvThreshold( im_gray, im_gray, 30, 255, CV_THRESH_BINARY );
-
-  cvCvtColor(im_gray,imgOld,CV_GRAY2RGB);
-
-  cvNot(imgOld,imgOld);
-
-  cvSub(imgNew,imgOld,imgOld);
-
-  cout<<"Original size: "<<imgNew->imageSize<<endl;
-        //Mat src = imread("photo.bmp");
-  Mat src(imgOld);
-
-        //jpeg compression
-        vector<uchar> buff;//buffer for coding
-        vector<int> param = vector<int>(2);
-        param[0]=CV_IMWRITE_JPEG_QUALITY;
-        param[1]=20;//default(95) 0-100
-        imencode(".jpg",src,buff,param);
-        cout<<"JPEG size: "<<buff.size()<<endl;
-
-  Mat jpegimage = imdecode(Mat(buff),CV_LOAD_IMAGE_COLOR);
-  IplImage dst_img = jpegimage;
-
-     cvShowImage( "Example2", &dst_img );
-  //frame1=frame2;
-
-     char c = cvWaitKey(50);
-     if( c == 27 ) break;
-  frame = cvQueryFrame( capture );
-   }
-   cvReleaseCapture( &capture );
-   cvDestroyWindow( "Example2" );
+    char decimal_port[16];
+    snprintf(decimal_port, sizeof(decimal_port), "%d", f_port);
+    decimal_port[sizeof(decimal_port) / sizeof(decimal_port[0]) - 1] = '\0';
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
+    int r(getaddrinfo(addr.c_str(), decimal_port, &hints, &f_addrinfo));
+    if(r != 0 || f_addrinfo == NULL)
+    {
+        throw udp_client_server_runtime_error(("invalid address or port for UDP socket: \"" + addr + ":" + decimal_port + "\"").c_str());
+    }
+    f_socket = socket(f_addrinfo->ai_family, SOCK_DGRAM | SOCK_CLOEXEC, IPPROTO_UDP);
+    if(f_socket == -1)
+    {
+        freeaddrinfo(f_addrinfo);
+        throw udp_client_server_runtime_error(("could not create UDP socket for: \"" + addr + ":" + decimal_port + "\"").c_str());
+    }
+    r = bind(f_socket, f_addrinfo->ai_addr, f_addrinfo->ai_addrlen);
+    if(r != 0)
+    {
+        freeaddrinfo(f_addrinfo);
+        close(f_socket);
+        throw udp_client_server_runtime_error(("could not bind UDP socket with: \"" + addr + ":" + decimal_port + "\"").c_str());
+    }
 }
-*/
+
+/** \brief Clean up the UDP server.
+ *
+ * This function frees the address info structures and close the socket.
+ */
+udp_server::~udp_server()
+{
+    freeaddrinfo(f_addrinfo);
+    close(f_socket);
+}
+
+/** \brief The socket used by this UDP server.
+ *
+ * This function returns the socket identifier. It can be useful if you are
+ * doing a select() on many sockets.
+ *
+ * \return The socket of this UDP server.
+ */
+int udp_server::get_socket() const
+{
+    return f_socket;
+}
+
+/** \brief The port used by this UDP server.
+ *
+ * This function returns the port attached to the UDP server. It is a copy
+ * of the port specified in the constructor.
+ *
+ * \return The port of the UDP server.
+ */
+int udp_server::get_port() const
+{
+    return f_port;
+}
+
+/** \brief Return the address of this UDP server.
+ *
+ * This function returns a verbatim copy of the address as passed to the
+ * constructor of the UDP server (i.e. it does not return the canonalized
+ * version of the address.)
+ *
+ * \return The address as passed to the constructor.
+ */
+std::string udp_server::get_addr() const
+{
+    return f_addr;
+}
+
+/** \brief Wait on a message.
+ *
+ * This function waits until a message is received on this UDP server.
+ * There are no means to return from this function except by receiving
+ * a message. Remember that UDP does not have a connect state so whether
+ * another process quits does not change the status of this UDP server
+ * and thus it continues to wait forever.
+ *
+ * Note that you may change the type of socket by making it non-blocking
+ * (use the get_socket() to retrieve the socket identifier) in which
+ * case this function will not block if no message is available. Instead
+ * it returns immediately.
+ *
+ * \param[in] msg  The buffer where the message is saved.
+ * \param[in] max_size  The maximum size the message (i.e. size of the \p msg buffer.)
+ *
+ * \return The number of bytes read or -1 if an error occurs.
+ */
+int udp_server::recv(char *msg, size_t max_size)
+{
+    return ::recv(f_socket, msg, max_size, 0);
+}
+
+/** \brief Wait for data to come in.
+ *
+ * This function waits for a given amount of time for data to come in. If
+ * no data comes in after max_wait_ms, the function returns with -1 and
+ * errno set to EAGAIN.
+ *
+ * The socket is expected to be a blocking socket (the default,) although
+ * it is possible to setup the socket as non-blocking if necessary for
+ * some other reason.
+ *
+ * This function blocks for a maximum amount of time as defined by
+ * max_wait_ms. It may return sooner with an error or a message.
+ *
+ * \param[in] msg  The buffer where the message will be saved.
+ * \param[in] max_size  The size of the \p msg buffer in bytes.
+ * \param[in] max_wait_ms  The maximum number of milliseconds to wait for a message.
+ *
+ * \return -1 if an error occurs or the function timed out, the number of bytes received otherwise.
+ */
+int udp_server::timed_recv(char *msg, size_t max_size, int max_wait_ms)
+{
+    fd_set s;
+    FD_ZERO(&s);
+    FD_SET(f_socket, &s);
+    struct timeval timeout;
+    timeout.tv_sec = max_wait_ms / 1000;
+    timeout.tv_usec = (max_wait_ms % 1000) * 1000;
+    int retval = select(f_socket + 1, &s, &s, &s, &timeout);
+    if(retval == -1)
+    {
+        // select() set errno accordingly
+        return -1;
+    }
+    if(retval > 0)
+    {
+        // our socket has data
+        return ::recv(f_socket, msg, max_size, 0);
+    }
+
+    // our socket has no data
+    errno = EAGAIN;
+    return -1;
+}
+
+int main(int argc, char const *argv[]) {
+
+  udp_server image_server("192.168.10.6",1234);
+  char msg[1024];
+  while (1) {
+    /* code */
+    if(image_server.recv(msg,1024)>0){
+      std::cout << msg << '\n';
+    }
+
+  }
+
+
+  return 0;
+}
